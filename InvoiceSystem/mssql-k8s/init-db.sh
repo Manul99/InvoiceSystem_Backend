418e5df56fc1f68014215278f5d071577ff01716
Manul99 <114052195+Manul99@users.noreply.github.com>
2025-06-22 10:29:20 +0530

    Added kubenetes configurations

---
+++ InvoiceSystem/mssql-k8s/init-db.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+# Wait for SQL Server to start
+echo "Waiting for SQL Server to start..."
+sleep 20
+
+# Run the SQL script
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P YourPassword123 -i /init/invoiceAppDB.sql
